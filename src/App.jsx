4c2f25351b985935bc070397a8516d00ad917088
Samson Ameh <amehsam69@gmail.com>
2024-04-24 18:03:55 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,17 @@ import Tours from "./Tours";
 
 const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  const [tours, setTours] = useState([]);
+
+  async function fetchTours() {
+    const response = await fetch(url);
+    const data = await response.json();
+    setTours(data);
+  }
+
+  fetchTours();
+
+  return <h2>{tours.name}</h2>;
 }
 
 export default App;
